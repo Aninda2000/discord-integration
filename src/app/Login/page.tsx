1923871a27507cf31325979a22e6f62333f3c61e
Aninda2000 <anindaroy100@gmail.com>
2023-08-12 09:22:45 +0530

    change-- the base url

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const LoginPage: React.FC = () => {
   const handleLogin = () => {
     const clientId = '1139069996178341979';
-    const redirectUri = encodeURIComponent('https://discord-demo-seven.vercel.app/auth');
+    const redirectUri = encodeURIComponent(`${window.location.origin}/auth`);
     const scopes = encodeURIComponent('identify email guilds');
     const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=${scopes}`;
 
