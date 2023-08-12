fb2106975ec7d5612171274f1d5f2ce0c45a3267
Aninda2000 <anindaroy100@gmail.com>
2023-08-12 09:05:40 +0530

    deploy --- in vercel

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const LoginPage: React.FC = () => {
   const handleLogin = () => {
     const clientId = '1139069996178341979';
-    const redirectUri = encodeURIComponent('http://localhost:3000/auth');
+    const redirectUri = encodeURIComponent('https://discord-demo-seven.vercel.app/auth');
     const scopes = encodeURIComponent('identify email guilds');
     const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=${scopes}`;
 
